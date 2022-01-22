9870dc5ed780909fc2ca8ced95e3f48d81744b80
SK <sinan-koparan@outlook.com>
2022-01-22 16:41:43 +1100

    Added comments to the class and methods.

--- src/inventorymanager/InventoryPreset.java
+++ src/inventorymanager/InventoryPreset.java
@@ -6,37 +6,63 @@ import org.dreambot.api.methods.container.impl.bank.Bank;
 
 import java.util.ArrayList;
 
+//*********************************************************************************
+// CLASS: InventoryPreset
+// The purpose of this class is to create a preset of items that the local player
+// can withdraw from the bank.
+//*********************************************************************************
 public class InventoryPreset {
 
     private ArrayList<String> itemName = new ArrayList<String>();
     private String[] inventoryItemArray = new String[28];
     private int[] inventoryQuantityArray = new int[28];
 
+    //*********************************************************************************
+    // CONSTRUCTOR: InventoryPreset Constructor
+    // Takes parameters of inventoryItemArray and inventoryQuantityArray
+    // One array is used to retrieve items and the other is used to get the quantity of items
+    //*********************************************************************************
     public InventoryPreset(String[] inventoryItemArray, int[] inventoryQuantityArray) {
         this.inventoryItemArray = inventoryItemArray;
         this.inventoryQuantityArray = inventoryQuantityArray;
     }
 
+    //*********************************************************************************
+    // ACCESSOR: getInventoryArray()
+    // Gets the inventoryItemArray
+    //*********************************************************************************
     public String[] getInventoryArray() {
         return this.inventoryItemArray;
     }
 
+    //*********************************************************************************
+    // MUTATOR: getInventoryArray()
+    // Sets the inventoryItemArray
+    //*********************************************************************************
     public void setInventoryArray(String[] setInventoryArray) {
         this.inventoryItemArray = setInventoryArray;
     }
 
+    //*********************************************************************************
+    // ACCESSOR: getInventoryQuantityArray()
+    // Gets the getInventoryQuantityArray
+    //*********************************************************************************
     public int[] getInventoryQuantityArray() {
         return this.inventoryQuantityArray;
     }
 
+    //*********************************************************************************
+    // MUTATOR: setInventoryQuantityArray()
+    // Sets the inventoryItemArray
+    //*********************************************************************************
     public void setInventoryQuantityArray(int[] setInventoryQuantityArray) {
         this.inventoryQuantityArray = setInventoryQuantityArray;
     }
 
-    public int getInventoryArrayLength() {
-        return inventoryItemArray.length;
-    }
-
+    //*********************************************************************************
+    // VOID METHOD: withdrawPresetInventory()
+    // Withdraws the preset items from the local player's bank
+    //*********************************************************************************
     public void withdrawPresetInventory() {
         for (int i = 0; i < inventoryItemArray.length; i++) {
             String itemName = inventoryItemArray[i];
@@ -46,6 +72,10 @@ public class InventoryPreset {
         }
     }
 
+    //*********************************************************************************
+    // RETURN METHOD: toString()
+    // Neatly prints out the preset information.
+    //*********************************************************************************
     public String toString() {
         StringBuilder inventoryDetails = new StringBuilder();
         for (int i = 0; i < inventoryItemArray.length; i++) {
